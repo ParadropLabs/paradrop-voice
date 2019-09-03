667619715756ff07a1c5844fbf5099c6cfc2f5ca
Lance Hartung <hartung@cs.wisc.edu>
2019-09-03 12:52:37 -0500

    Upgrade dependencies to fix vulnerabilities.

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ setup(name="paradrop-voice",
       url="https://paradrop.org",
       packages=find_packages(),
       install_requires=[
-          "certifi==2018.4.16",
-          "chardet==3.0.4",
-          "Flask==1.0.2",
-          "idna==2.6",
-          "pocketsphinx==0.1.3",
-          "pyttsx3==2.7",
-          "requests==2.18.4",
-          "urllib3==1.22"
+          "certifi>=2019.6.16",
+          "chardet>=3.0.4",
+          "Flask>=1.0.2",
+          "idna>=2.8",
+          "pocketsphinx>=0.1.3",
+          "pyttsx3>=2.7",
+          "requests>=2.22.0",
+          "urllib3>=1.25.3",
+          "Werkzeug>=0.15.5"
       ],
       entry_points={
           "console_scripts": [
